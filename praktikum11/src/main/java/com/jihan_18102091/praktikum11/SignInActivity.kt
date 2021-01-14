7daea57bcd9f8548ff3d5b444169111957895318
jihnrhd <72547450+jihnrhd@users.noreply.github.com>
2021-01-14 14:33:23 +0700

    Connect with firebase, add SignUpActivity and SignInActivity

---
+++ praktikum11/src/main/java/com/jihan_18102091/praktikum11/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.jihan_18102091.praktikum11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
