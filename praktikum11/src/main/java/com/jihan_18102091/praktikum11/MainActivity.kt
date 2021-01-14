a84945c2883fa52c76b40b10f39c3e2fadb67898
jihnrhd <72547450+jihnrhd@users.noreply.github.com>
2021-01-14 15:48:40 +0700

    Editing SignUpActivity and SignInActivity pt2

--- praktikum11/src/main/java/com/jihan_18102091/praktikum11/MainActivity.kt
+++ praktikum11/src/main/java/com/jihan_18102091/praktikum11/MainActivity.kt
@@ -3,9 +3,12 @@ package com.jihan_18102091.praktikum11
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.Toast
+import androidx.core.view.isVisible
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.jihan_18102091.praktikum11.databinding.ActivityMainBinding
@@ -25,8 +28,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             startActivity(intent)
             finish()
         }
+        else{
+            updateUI(currentUser)
+        }
         binding.btnSignOut.setOnClickListener(this)
         binding.btnEmailVerify.setOnClickListener(this)
+
     }
     public override fun onStart() {
         super.onStart()
@@ -35,6 +42,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             val intent = Intent(this@MainActivity, SignInActivity::class.java)
             startActivity(intent)
             finish()
+        }
+        else{
+            updateUI(currentUser)
         }
     }
     override fun onClick(v: View) {
@@ -73,5 +83,29 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             finish()
         }
     }
-
+    private fun updateUI(currentUser: FirebaseUser) {
+        currentUser?.let {
+            val name = currentUser.displayName
+            val phoneNumber = currentUser.phoneNumber
+            val email = currentUser.email
+            val photoUrl = currentUser.photoUrl
+            val emailVerified = currentUser.isEmailVerified
+            val uid = currentUser.uid
+            binding.tvName.text = name
+            if(TextUtils.isEmpty(name)){
+                binding.tvName.text = "No Name"
+            }
+            binding.tvUserId.text = email
+            for (profile in it.providerData) {
+                val providerId = profile.providerId
+                if(providerId=="password" && emailVerified==true){
+                    binding.btnEmailVerify.isVisible = false
+                }
+                if(providerId=="phone"){
+                    binding.tvName.text = phoneNumber
+                    binding.tvUserId.text = providerId
+                }
+            }
+        }
+    }
 }
