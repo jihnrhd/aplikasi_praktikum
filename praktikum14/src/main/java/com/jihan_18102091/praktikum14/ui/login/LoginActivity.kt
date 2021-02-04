da69a9adf286b64dd9e87a0e1f8fa8df87e6719b
jihnrhd <72547450+jihnrhd@users.noreply.github.com>
2021-02-04 16:37:19 +0700

    Persiapan UI 2

---
+++ praktikum14/src/main/java/com/jihan_18102091/praktikum14/ui/login/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.jihan_18102091.praktikum14.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
