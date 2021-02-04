1431f364ac4d9402b377d163da13575623bac220
jihnrhd <72547450+jihnrhd@users.noreply.github.com>
2021-02-04 17:25:04 +0700

    Konfigurasi Token

--- praktikum14/src/main/java/com/jihan_18102091/praktikum14/MainActivity.kt
+++ praktikum14/src/main/java/com/jihan_18102091/praktikum14/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.jihan_18102091.praktikum14
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.jihan_18102091.praktikum14.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tokenPref: TokenPref
+    private lateinit var token: Token
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +27,12 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_my_quotes, R.id.navigation_class_quotes, R.id.navigation_global_quotes))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        tokenPref = TokenPref(this)
+        token = tokenPref.getToken()
+        if (TextUtils.isEmpty(token.token)) {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
